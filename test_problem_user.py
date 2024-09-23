60614d4ff85d0e36a2bbde685769d0ba438ecfa7
Aleksandr <aleksandrsemeonow@yandex.ru>
2024-09-23 15:16:27 +0500

    test add all products in basket

--- test_problem_user.py
+++ test_problem_user.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 URI = "http://www.saucedemo.com"
 user_name = 'problem_user'
@@ -36,3 +37,31 @@ def test_identical_images_by_products(driver):
 
     # Проверяем, что все значения src одинаковы
     assert len(set(image_sources)) == 1
+
+def test_add_all_products_in_basket_and_remove(driver):
+    """ Тест на добавление всех товаров в корзину, а после удаления их со страницы каталога """
+
+    # Авторизация
+    authorization(driver)
+
+    # Добавляем все товары в корзину
+    all_btn_add_product = driver.find_elements(By.CLASS_NAME, "btn_inventory")
+    for btn in all_btn_add_product:
+        btn.click()
+
+    # Всего должно быть 6 кнопок
+    assert len(all_btn_add_product) == 6
+
+    # Проверка на то что не все товары добавились в корзину
+    badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+    assert badge.text != "6"
+
+    # Пробуем удалить все товары при нажатии на кнопку remove
+    all_btn_add_product = driver.find_elements(By.CLASS_NAME, "btn_inventory")
+    for btn in all_btn_add_product:
+        if "Remove" in btn.text:
+            btn.click()
+
+    # Проверка на то что не все товары удалились из корзины и корзина не пустая
+    badge = driver.find_element(By.CLASS_NAME, "shopping_cart_badge")
+    assert badge.text != "0"
